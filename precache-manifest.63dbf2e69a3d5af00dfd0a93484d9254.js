a02ab07f0f4f4abb3493afee1dbdf15fccf87acc
pershantgoel <pershantgoel@gmail.com>
2019-06-04 19:41:52 +0530

    Updates

---
+++ precache-manifest.63dbf2e69a3d5af00dfd0a93484d9254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8b8278c393c28627f5063ae29b6136",
+    "url": "/recipeApp/index.html"
+  },
+  {
+    "revision": "28ea12cfa448cd1bfe96",
+    "url": "/recipeApp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "eba39d076ddcc15f133c",
+    "url": "/recipeApp/static/css/main.45ef724b.chunk.css"
+  },
+  {
+    "revision": "28ea12cfa448cd1bfe96",
+    "url": "/recipeApp/static/js/2.848c973a.chunk.js"
+  },
+  {
+    "revision": "eba39d076ddcc15f133c",
+    "url": "/recipeApp/static/js/main.c83e7e71.chunk.js"
+  },
+  {
+    "revision": "42074dc743b567092783",
+    "url": "/recipeApp/static/js/runtime~main.08bbce9c.js"
+  }
+]);
